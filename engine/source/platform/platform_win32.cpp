054be473dfaadb887170235398ae82b8fc91b4e8
Riley Hawksworth <rileyrooh13@gmail.com>
2024-03-24 20:12:54 +0000

    Move logging colours to windows platform layer.

--- engine/source/platform/platform_win32.cpp
+++ engine/source/platform/platform_win32.cpp
@@ -15,12 +15,20 @@ namespace Platform {
     }
 
     namespace Console {
+        uint8 LoggerColours[5] = {
+            BACKGROUND_RED | 0,                 // FATAL
+            FOREGROUND_RED,                     // ERROR 
+            FOREGROUND_RED | FOREGROUND_GREEN,  // WARN
+            FOREGROUND_GREEN,                   // DEBUG
+            FOREGROUND_INTENSITY                // INFO
+        };
+
         void Write(std::string text, uint8 colour) {
             HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
             uint64 length = strlen(text.c_str());
             LPDWORD number_written = 0;
 
-            SetConsoleTextAttribute(console_handle, colour);
+            SetConsoleTextAttribute(console_handle, LoggerColours[colour]);
             WriteConsoleA(console_handle, text.c_str(), (DWORD)length, number_written, 0);
 
         }
@@ -30,7 +38,7 @@ namespace Platform {
             uint64 length = strlen(text.c_str());
             LPDWORD number_written = 0;
 
-            SetConsoleTextAttribute(console_handle, colour);
+            SetConsoleTextAttribute(console_handle, LoggerColours[colour]);
             WriteConsoleA(console_handle, text.c_str(), (DWORD)length, number_written, 0);
         }
     }
--- sandbox/source/main.cpp
+++ sandbox/source/main.cpp
@@ -5,20 +5,13 @@
 
 int main()
 {
-    // TODO: Move the logging colours to platform layer.
-    uint8 fatal_colour = BACKGROUND_RED | 0;
-    uint8 error_colour = FOREGROUND_RED;
-    uint8 warn_colour = FOREGROUND_RED | FOREGROUND_GREEN;
-    uint8 debug_colour = FOREGROUND_GREEN;
-    uint8 info_colour = FOREGROUND_INTENSITY;
-
     // TODO: Move the logging over to a custom logger
     // rather than doing it this way.
-    Platform::Console::Write("Fatal message.\n", fatal_colour);
-    Platform::Console::Write("Error message.\n", error_colour);
-    Platform::Console::Write("Warn message.\n", warn_colour);
-    Platform::Console::Write("Debug message.\n", debug_colour);
-    Platform::Console::Write("Info message.\n", info_colour);
+    Platform::Console::Write("Fatal message.\n", 0);
+    Platform::Console::Write("Error message.\n", 1);
+    Platform::Console::Write("Warn message.\n", 2);
+    Platform::Console::Write("Debug message.\n", 3);
+    Platform::Console::Write("Info message.\n", 4);
 
     return EXIT_SUCCESS;
 }
