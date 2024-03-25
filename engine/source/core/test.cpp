a90d6e2cb4008f7f1d6eef7db8ba88d77ac1fb98
Riley Hawksworth <rileyrooh13@gmail.com>
2024-03-25 15:24:23 +0000

    Remove test file from engine.

--- engine/source/core/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.hpp"
-
-namespace Test {
-    void PrintInt(int64 number) {
-        std::cout << "The 64-bit integer is: " << number << std::endl;
-    }
-}
--- engine/source/core/test.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../defines.hpp"
-
-#include <iostream>
-
-namespace Test {
-    GRAPE_API void PrintInt(int64 number);
-}
