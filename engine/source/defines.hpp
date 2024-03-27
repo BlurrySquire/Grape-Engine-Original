60a9113c2adf5738dc192770651ece381330b605
Riley Hawksworth <rileyrooh13@gmail.com>
2024-03-27 19:15:06 +0000

    Update types in 'defines.hpp'.

--- engine/source/defines.hpp
+++ engine/source/defines.hpp
@@ -14,21 +14,21 @@
 // to detect what platform it is being ran on.
 
 // Common Variable Types
-using uint8 = unsigned char;
-using uint16 = unsigned short;
-using uint32 = unsigned int;
-using uint64 = unsigned long long;
+#include <cstdint>
 
-using int8 = signed char;
-using int16 = signed short;
-using int32 = signed int;
-using int64 = signed long long;
+using uint8 = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+
+using int8 = int8_t;
+using int16 = int16_t;
+using int32 = int32_t;
+using int64 = int64_t;
 
 using float32 = float;
 using float64 = double;
-
-#define TRUE 1
-#define FALSE 0
+using float128 = __float128;
 
 // Static Assertion
 #if defined(__clang__) || defined(__gcc__)
