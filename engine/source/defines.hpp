2b4ee1832671a34fc3263334fe83f5e8ff0113d3
Riley Hawksworth <rileyrooh13@gmail.com>
2024-03-28 17:17:42 +0000

    Remove custom variable types, using cpp standard lib types instead.

--- engine/source/defines.hpp
+++ engine/source/defines.hpp
@@ -16,20 +16,6 @@
 // Common Variable Types
 #include <cstdint>
 
-using uint8 = uint8_t;
-using uint16 = uint16_t;
-using uint32 = uint32_t;
-using uint64 = uint64_t;
-
-using int8 = int8_t;
-using int16 = int16_t;
-using int32 = int32_t;
-using int64 = int64_t;
-
-using float32 = float;
-using float64 = double;
-using float128 = __float128;
-
 // Static Assertion
 #if defined(__clang__) || defined(__gcc__)
 #define STATIC_ASSERT _Static_assert
