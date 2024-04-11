6d93591d5d85fff53b189414b16b921d8db58542
Riley Hawksworth <114335081+BlurrySquire@users.noreply.github.com>
2024-04-11 17:54:09 +0100

    Fix a few errors from not merging correctly.

--- scripts/Generate_VS2022_Solution.bat
+++ scripts/Generate_VS2022_Solution.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 pushd %~dp0\..\
-call 3rd-party\!Premake\Windows\premake5.exe --file=premake5.lua vs2022
+call 3rd-Party\!Premake\Windows\premake5.exe --file=premake5.lua vs2022
 popd
 PAUSE
